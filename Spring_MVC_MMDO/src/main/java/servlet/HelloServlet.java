2d2d435017a25c6165a31578b2d8a6f1b4c2e2b8
Mykhajlo Drach <qwewerty12@gmail.com>
2018-05-12 15:03:58 +0300

    Init

--- Spring_MVC_MMDO/src/main/java/servlet/HelloServlet.java
+++ Spring_MVC_MMDO/src/main/java/servlet/HelloServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
         name = "MyServlet",
-        urlPatterns = {"/"}
+        urlPatterns = {"/hello"}
 )
 public class HelloServlet extends HttpServlet {
 
